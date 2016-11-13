3d5f34adb4b5bc44c45896dbfa115053fc47ab71
mdsecurity <mattiasduffy@MacBook-Woah.local>
2016-11-13 10:21:43 -0500

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
 var object = [["gina",-1]];
 
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/css'));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
