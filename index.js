83f8bb103569038fde5ab4e7b6a8d6e159dc860e
mdsecurity <mattiasduffy@MacBook-Woah.local>
2016-11-13 08:43:14 -0500

    removing canvas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var object = [["gina",-1]]
+var object = [["gina",-1]];
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
